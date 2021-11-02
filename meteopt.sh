0988829104527a61c42d0afe35e5d97014d0ac61
shaulliv <shaulliv@gmail.com>
2021-11-02 14:33:08 +0200

    Update meteopt.sh

--- meteopt.sh
+++ meteopt.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# command from HTML page to text file
 curl -s http://81.218.166.163:3803/EnvErmPublish15/secure_members/TabularDetailsView.aspx?Device=a30 > /tmp/ptmeteostation.txt
+# Temperature
 temp=$(cat /tmp/ptmeteostation.txt | grep ctl00_ContentPlaceHolder1_gdvTabularMonitorsHistory_cell0_19_ctl00_ctl01_VIC | sed 's/.*">//' | sed 's/<.*$//')
+# Wind direction
 wd=$(cat /tmp/ptmeteostation.txt | grep ctl00_ContentPlaceHolder1_gdvTabularMonitorsHistory_tccell0_19 | sed "s/.*'>//g" | sed 's/<.*$//')
+# Relative Humidity
 rh=$(cat /tmp/ptmeteostation.txt | grep ctl00_ContentPlaceHolder1_gdvTabularMonitorsHistory_cell0_18_ctl00_ctl01_VIC | sed 's/.*">//g' | sed 's/<.*$//')
+# Rain (Daily)
 rain=$(cat /tmp/ptmeteostation.txt | grep ctl00_ContentPlaceHolder1_gdvTabularMonitorsHistory_cell0_17_ctl00_ctl01_VIC | sed 's/.*">//g' | sed 's/<.*$//')
+# Wind speed
 ws=$(cat /tmp/ptmeteostation.txt | grep ctl00_ContentPlaceHolder1_gdvTabularMonitorsHistory_tccell0_61 | cut -f6 -d'>' | sed 's/<.*$//')
